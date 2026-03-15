b6385f384bb0dee701ad6c42456d2fff9ffd7dba
veracityman1-commits <veracityman1@gmail.com>
2026-03-15 18:10:47 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE = 'family-app-v1';
+var FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
